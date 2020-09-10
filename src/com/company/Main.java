715b49b29873afb67f6a5a2ec2b5a7dd624b8f6e
Anders Wenström <anders.wenstrom@student.kyh.se>
2020-09-10 12:27:48 +0200

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Mean of 1, 1, 3, 3: " + meanCalc ( 1,  1,  3,  3));
+        System.out.println("Mean of 1, 2, 3, 4: " + meanCalc (1, 2, 3, 4));
+        System.out.println("Mean of 1, -2, 3, -4: " + meanCalc (1, -2, 3, -4));
+        System.out.println("Mean of -1, -11, 24, 2: " + meanCalc (-1, -11, 24, 2));
+    }
+
+            public static String meanCalc(int a, int b, int c, int d) {
+                double mean = (a + b + c + d) / 4;
+                String meanString = Double.toString(mean);
+
+                return meanString;
+        }
+    }
